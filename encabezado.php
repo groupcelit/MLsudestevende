480b68c945893a97975dbbc10f986ec96a54f66d
miamius <jairo.laupa@gmail.com>
2017-06-14 20:13:52 +0000

    Se suben arreglos con respecto al cart shopping

--- encabezado.php
+++ encabezado.php
@@ -8,7 +8,7 @@
 	<!--<link rel="stylesheet" href="assets/assets/css/bootstrap.min.css" />-->
 	<link rel="stylesheet" href="assets/css/reset.css" />
 	<link rel="stylesheet" href="assets/css/animate.min.css" />
-	<!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">-->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 	<!--<link rel="stylesheet" href="assets/css/bootstrap.min.css" />-->
 	<link rel="stylesheet" href="assets/css/font-awesome.min.css" />
 	<link rel="stylesheet" href="assets/css/socials.css" />
