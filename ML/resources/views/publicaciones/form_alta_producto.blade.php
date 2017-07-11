9e9327741f20ba259617ebd4e58f7d6f31fadc8f
miamius <jairo.laupa@gmail.com>
2017-07-11 14:47:07 -0300

    Se salta la validacion de foto vacia

--- ML/resources/views/publicaciones/form_alta_producto.blade.php
+++ ML/resources/views/publicaciones/form_alta_producto.blade.php
@@ -125,12 +125,14 @@
 								if($('[type=file]')[0].files[0] != undefined) {
 									$.each($('[type=file]'), function (e, file) {
 										/*2mb*/
-										if (file.files[0].size > 2000000) {
-											$('#msg').removeClass();
-											$('#msg').addClass('alert alert-warning');
-											$('#msg').html('Esta imagen pesa mas de 2mb :C ' + file.files[0].name);
-											$('#msg').focus();
-											return false;
+										if (file.files[0]!= undefined) {
+											if (file.files[0].size > 2000000) {
+												$('#msg').removeClass();
+												$('#msg').addClass('alert alert-warning');
+												$('#msg').html('Esta imagen pesa mas de 2mb :C ' + file.files[0].name);
+												$('#msg').focus();
+												return false;
+											}
 										}
 									});
 								}else{
@@ -153,13 +155,26 @@
 												console.log('enviado datos');
 									},
 								    success: function (response) {
-									console.log(response);
-									click=false;
-									$('#msg').removeClass();
-									$('#msg').addClass('alert alert-success');
-									$('#msg').html('Gracias por publicar');
-									$('#msg').focus();
-									$('#save').remove();
+												if(response.exito) {
+													click = false;
+													$('#msg').removeClass();
+													$('#msg').addClass('alert alert-success');
+													$('#msg').html('Gracias por publicar');
+													$('#msg').focus();
+													$('#save').remove();
+													setTimeout(
+															function(){
+																window.location.href = "/mis_anuncios"
+															}
+													,1500)
+
+												}else{
+													click = false;
+													$('#msg').removeClass();
+													$('#msg').addClass('alert alert-warning');
+													$('#msg').html('Ops ocurrio un error inesperado');
+													$('#msg').focus();
+												}
 								}
 							};
 							var url = 'anuncios/new_anuncio';
